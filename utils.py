101548c02d296a41688aa6d11a4f65e5c0720de1
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2024-01-14 20:33:54 +0530

    Added parse_pdf

--- utils.py
+++ utils.py
@@ -1,5 +1,21 @@
 from pypdf import PdfReader
 from langchain.docstore.document import Document
 
+def parse_pdf(file):
+    pdf = PdfReader
+    output = []
+    for page in pdf.pages:
+        text = page.extract_content()
+        # Merge hyphenated words
+        text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+        # Fix newlines in the middle of sentences
+        text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+        # Remove multiple newlines
+        text = re.sub(r"\n\s*\n", "\n\n", text)
+
+        output.append(text)
+
+    return output
+
 
     
