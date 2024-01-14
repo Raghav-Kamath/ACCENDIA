3e0e0c18e38587f6e2f093b76b9e8866a6c02d6f
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2024-01-14 20:37:50 +0530

    Added text to doc_chunks

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from pypdf import PdfReader
 from langchain.docstore.document import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def parse_pdf(file):
     pdf = PdfReader
@@ -17,5 +18,34 @@ def parse_pdf(file):
 
     return output
 
-
+def text_to_docs(text):
+    if isinstance(text, str):
+        text = [text]
     
+    page_docs = [Document(page_content=page) for page in text]
+
+    for i, doc in enumerate(page_docs):
+        doc.metadata["page"] = i + 1
+
+    doc_chunks = []
+
+    for doc in page_docs:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 800,
+            separators = ["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+            chunk_overlap = 0, 
+        )
+
+        chunks = text_splitter.split_text(doc.page_content)
+        for i, chunk in enumerate(chunks):
+            doc = Document(
+                page_content = chunk,
+                metadata={
+                    "page": doc.metadata["page"],
+                    "chunk": i,
+                }
+            ) 
+            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
+            doc_chunks.append(doc)   
+    
+    return doc_chunks
