18fa75970d38456ec19dda7104ee22a945c8062e
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2024-03-15 10:12:41 +0530

    Deleted config file

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SECRET_KEY = "4))3ND!A"
-os.environ['OPENAI_API_KEY'] = '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'
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyCzzb1pyA2rJJV6xXiP5qIHrIEscHeqaJo'
